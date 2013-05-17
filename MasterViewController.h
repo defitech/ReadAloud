63703178eeb66c4fd75957900caf4576956362de
maximerchrd <dev@iMac-de-adherent.local>
2013-05-17 17:13:39 +0200

    first step towards spell checking

--- MasterViewController.h
+++ MasterViewController.h
@@ -13,12 +13,14 @@
     IBOutlet NSSlider *speechSpeedSlider;
     IBOutlet  NSTextView *instructions;
     IBOutlet NSButton *buttonHideInstructions;
+    NSURL *urlForSavedFile;
 }
 
 @property (nonatomic, retain) NSSpeechSynthesizer *synth;
 @property (nonatomic, retain) IBOutlet NSSlider *speechSpeedSlider;
 @property (nonatomic, retain) IBOutlet NSTextView *instructions;
 @property (nonatomic, retain) IBOutlet NSButton *buttonHideInstructions;
+@property (nonatomic, retain) NSURL *urlForSavedFile;
 
 - (IBAction)buttonStartStopPressed:(id)sender;
 - (IBAction)valueChangedForspeechSpeedSlider:(id)sender;
--- MasterViewController.m
+++ MasterViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MasterViewController.h"
+#import "wav_to_flac.h"
+#import <AudioToolbox/AudioConverter.h>
 
 @interface MasterViewController ()
 
@@ -14,7 +16,7 @@
 
 @implementation MasterViewController
 
-@synthesize synth, speechSpeedSlider, instructions, buttonHideInstructions;
+@synthesize synth, speechSpeedSlider, instructions, buttonHideInstructions, urlForSavedFile;
 
 bool programON;
 NSTask *task;
@@ -28,15 +30,20 @@ float sliderFloatValue;
         //[synth setDelegate:synth.delegate]; //useful?
         [speechSpeedSlider setFloatValue:[synth rate]];
     }
-    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *appSettingsPath = [documentsDirectory stringByAppendingPathComponent:@"testAudio.aiff"];
+    //NSURL *url=[[NSURL alloc]initFileURLWithPath:appSettingsPath];
+    self.urlForSavedFile = [[NSURL alloc] initFileURLWithPath:appSettingsPath];
     programON = false;
     task = nil;
     sliderFloatValue = [synth rate];
     
+    
     return self;
 }
 
-
+//hello, how are you doing?
 - (IBAction)buttonStartStopPressed:(id)sender
 {
     if (programON == false) {
@@ -69,9 +76,76 @@ float sliderFloatValue;
                 [synth stopSpeaking];
             }
         } else if (copiedItems != nil && [copiedItems count] > 0 && [[copiedItems objectAtIndex:0] length] > 0 && ![PasteboardContent isEqualToString:[copiedItems objectAtIndex:0]]) {
-            [synth setRate:sliderFloatValue];
-            [synth startSpeakingString:[copiedItems objectAtIndex:0]];
-            //NSLog(@"ReadClipboard: %@, %@",PasteboardContent, [copiedItems objectAtIndex:0]);
+            //[synth setRate:sliderFloatValue];
+            //[synth startSpeakingString:[copiedItems objectAtIndex:0]];
+            if ([synth startSpeakingString:[copiedItems objectAtIndex:0] toURL:urlForSavedFile]) {
+                NSString *homeDirectory = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+                NSString *filePath = [NSString stringWithFormat:@"%@/%@", homeDirectory, @"testAudio.flac"];
+                NSLog(@"path:%@",filePath);
+                
+                
+                NSData *myData = [NSData dataWithContentsOfFile:filePath];
+                
+                NSMutableURLRequest *request = [[NSMutableURLRequest alloc]
+                                                initWithURL:[NSURL
+                                                             URLWithString:@"https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US"]];
+                
+                [request setHTTPMethod:@"POST"];
+                
+                //set headers
+                
+                [request addValue:@"Content-Type" forHTTPHeaderField:@"audio/x-flac; rate=16000"];
+                
+                [request addValue:@"audio/x-flac; rate=16000" forHTTPHeaderField:@"Content-Type"];
+                
+                [request setHTTPBody:myData];
+                
+                [request setValue:[NSString stringWithFormat:@"%ld",(unsigned long)[myData length]] forHTTPHeaderField:@"Content-length"];
+                
+                NSHTTPURLResponse* urlResponse = nil;
+                NSError *error = [[NSError alloc] init];
+                NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&error];
+                NSString *result = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+                
+                NSLog(@"The answer is: %@",result);
+                
+                /*NSData *myData = [NSData dataWithContentsOfFile:[NSString stringWithFormat:@"%@/recordTest.flac", urlForSavedFile]];
+                //NSString *audio = [NSString stringWithContentsOfFile:[NSString stringWithFormat:@"%@/recordTest.flac", recDir]];
+                
+                
+                
+                NSMutableURLRequest *request = [[NSMutableURLRequest alloc]
+                                                initWithURL:[NSURL
+                                                             URLWithString:@"https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US"]];
+                
+                
+                
+                
+                
+                
+                [request setHTTPMethod:@"POST"];
+                
+                //set headers
+                
+                [request addValue:@"Content-Type" forHTTPHeaderField:@"audio/x-flac; rate=16000"];
+                
+                [request addValue:@"audio/x-flac; rate=16000" forHTTPHeaderField:@"Content-Type"];
+                
+                NSString *requestBody = [[NSString alloc] initWithFormat:@"Content=%@", myData];
+                
+                [request setHTTPBody:[requestBody dataUsingEncoding:NSASCIIStringEncoding]];
+                
+                [request setValue:[NSString stringWithFormat:@"%ld",(unsigned long)[myData length]] forHTTPHeaderField:@"Content-length"];
+                
+                
+                
+                NSHTTPURLResponse* urlResponse = nil;  
+                NSError *error = [[NSError alloc] init];  
+                NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&error];  
+                NSString *result = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+                
+                NSLog(@"The answer is: %@",result);*/
+            } else NSLog(@"problem starting to speak out string");
             PasteboardContent = [copiedItems objectAtIndex:0];
         }
         if (copiedItems != nil && [copiedItems count] == 0) {
